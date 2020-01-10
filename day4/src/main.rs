594b200bf8bf38468920336409e4d3a223b28283
Benny Zlotnik <uber442@gmail.com>
2020-01-10 18:12:22 +0200

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,20 +1,35 @@
+use std::convert::TryFrom;
 use std::iter::FromIterator;
 
-fn only_increasing_and_adjacent(n: u32) -> bool {
+fn adjacent_pair(n: u32) -> bool {
     let v = Vec::from_iter(n.to_string().chars());
-    let mut adjacent = false;
+    let mut occ: [u32; 10] = [0; 10];
 
-    for i in 0..v.len() - 1 {
-        if v[i] > v[i + 1] {
-            return false;
-        }
+    for i in v {
+        // There has to be a better way...
+        let i = i.to_digit(10).unwrap();
+        occ[usize::try_from(i).unwrap()] += 1;
+    }
 
-        if v[i] == v[i + 1] {
-            adjacent = true;
+    for i in &occ {
+        if *i == 2 {
+            return true;
         }
     }
 
-    adjacent
+    false
+}
+
+fn only_increasing(n: u32) -> bool {
+    let v = Vec::from_iter(n.to_string().chars());
+
+    for i in 0..v.len() - 1 {
+        if v[i] as u32 > v[i + 1] as u32 {
+            return false;
+        }
+    }
+
+    true
 }
 
 fn check_number(n: u32) -> bool {
@@ -22,7 +37,11 @@ fn check_number(n: u32) -> bool {
         return false;
     }
 
-    if !only_increasing_and_adjacent(n) {
+    if !only_increasing(n) {
+        return false;
+    }
+
+    if !adjacent_pair(n) {
         return false;
     }
 
